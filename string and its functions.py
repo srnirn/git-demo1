83b231385bd65fb712367c9cb93d750318d4a075
srnirn <srnirn@amazon.com>
2021-04-02 06:43:55 -0700

    the first commit

---
+++ string and its functions.py
@@ -0,0 +1,15 @@
+str= "Rahul Shetty academy.com"
+str1= "Consulting firm"
+str3= "Rahul Shetty"
+print(str[1])
+print(str[0:5])
+print(str+str1)
+print(str3 in str)
+var= str.split(".")
+print(var)
+print(var[0])
+str4= " great "
+print(str4.strip())
+print(str4.lstrip())
+print(str4.rstrip())
+
